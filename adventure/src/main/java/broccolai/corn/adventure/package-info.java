6184662c99a73e29577c0fe01bdf03bee17ca56c
broccolai <me@broccol.ai>
2021-02-08 05:58:19 +0000

    Add adventure module

---
+++ adventure/src/main/java/broccolai/corn/adventure/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Adventure specific utilities, for Paper
+ */
+package broccolai.corn.adventure;
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,8 @@
-include("core", "spigot", "paper")
+include("core", "spigot", "paper", "adventure")
 
 rootProject.name = "corn-parent"
 
 findProject(":core")?.name = "corn-core"
 findProject(":spigot")?.name = "corn-spigot"
 findProject(":paper")?.name = "corn-paper"
+findProject(":adventure")?.name = "corn-adventure"
