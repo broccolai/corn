d3dc392cd0181dcedc4e4883c2f94573ffb0690a
broccolai <me@broccol.ai>
2021-02-18 16:17:38 +0000

    Move to @kyori/indra

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,17 +1,11 @@
-plugins {
-    id("java-library")
-}
-
 repositories {
-    maven("https://hub.spigotmc.org/nexus/content/repositories/snapshots/")
-    maven("https://oss.sonatype.org/content/repositories/public/")
     maven("https://papermc.io/repo/repository/maven-public/")
 }
 
 dependencies {
     api(project(":corn-spigot"))
 
-    compileOnlyApi("com.destroystokyo.paper:paper-api:1.16.4-R0.1-SNAPSHOT") {
+    compileOnlyApi("com.destroystokyo.paper", "paper-api", Versions.PAPER_VERSION) {
         exclude(module = "guava")
         exclude(module = "gson")
         exclude(module = "snakeyaml")
