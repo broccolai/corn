f35c127ce5e9028e11f16dd8e8a6ca75e026f5c2
broccolai <me@broccol.ai>
2021-05-22 23:24:00 +0100

    Allow DelegatingContextKeyRegistry#register to take a vararg of keys

--- context/src/main/java/broccolai/corn/context/DelegatingContextKeyRegistry.java
+++ context/src/main/java/broccolai/corn/context/DelegatingContextKeyRegistry.java
@@ -2,6 +2,7 @@ package broccolai.corn.context;
 
 import org.checkerframework.checker.nullness.qual.NonNull;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -10,8 +11,8 @@ public class DelegatingContextKeyRegistry implements ContextKeyRegistry {
 
     private final Set<ContextKey<?>> keys = new HashSet<>();
 
-    protected final void register(final @NonNull ContextKey<?> key) {
-        this.keys.add(key);
+    protected final void register(final @NonNull ContextKey<?>... keys) {
+        Collections.addAll(this.keys, keys);
     }
 
     @Override
