f8d0c9d250360a3c0f5813b42d72c5134de5a3df
broccolai <me@broccol.ai>
2021-06-13 21:34:10 +0100

    Add tests for complex properties

--- properties/src/test/java/broccolai/corn/properties/DirtyPropertiesMapTest.java
+++ properties/src/test/java/broccolai/corn/properties/DirtyPropertiesMapTest.java
@@ -9,7 +9,7 @@ public final class DirtyPropertiesMapTest {
 
     @Test
     void testEmptyDirty() {
-        DirtyPropertiesMap<Integer, SomePropertyHolder> dirtyMap = DirtyPropertiesMap.hashmap();
+        DirtyPropertiesMap<Integer, PropertyHolder> dirtyMap = DirtyPropertiesMap.hashmap();
         dirtyMap.put(1, new SomePropertyHolder(20, "word"));
         dirtyMap.put(5, new SomePropertyHolder(15, "wording"));
 
@@ -18,13 +18,64 @@ public final class DirtyPropertiesMapTest {
 
     @Test
     void testChangedDirty() {
-        DirtyPropertiesMap<Integer, SomePropertyHolder> dirtyMap = DirtyPropertiesMap.hashmap();
+        DirtyPropertiesMap<Integer, PropertyHolder> dirtyMap = DirtyPropertiesMap.hashmap();
         SomePropertyHolder toChange = new SomePropertyHolder(20, "word");
         dirtyMap.put(1, toChange);
         dirtyMap.put(5, new SomePropertyHolder(15, "wording"));
 
         toChange.number(25);
         assertThat(dirtyMap.dirty()).containsExactly(toChange);
+    }
+
+    @Test
+    void testChangedAndResetDirty() {
+        DirtyPropertiesMap<Integer, PropertyHolder> dirtyMap = DirtyPropertiesMap.hashmap();
+        SomePropertyHolder toChange = new SomePropertyHolder(20, "word");
+        dirtyMap.put(1, toChange);
+        dirtyMap.put(5, new SomePropertyHolder(15, "wording"));
+
+        toChange.number(20);
+        assertThat(dirtyMap.dirty()).isEmpty();
+    }
+
+    @Test
+    void testComplexEmptyDirty() {
+        DirtyPropertiesMap<Integer, PropertyHolder> dirtyMap = DirtyPropertiesMap.hashmap();
+        dirtyMap.put(1, new SomeComplexPropertyHolder(
+                20,
+                new SomePropertyHolder(10, "test")
+        ));
+        dirtyMap.put(5, new SomePropertyHolder(15, "wording"));
+
+        assertThat(dirtyMap.dirty()).isEmpty();
+    }
+
+    @Test
+    void testComplexChangedDirty() {
+        DirtyPropertiesMap<Integer, PropertyHolder> dirtyMap = DirtyPropertiesMap.hashmap();
+        SomeComplexPropertyHolder toChange = new SomeComplexPropertyHolder(
+                20,
+                new SomePropertyHolder(10, "test")
+        );
+        dirtyMap.put(1, toChange);
+        dirtyMap.put(5, new SomePropertyHolder(15, "wording"));
+
+        toChange.number(25);
+        assertThat(dirtyMap.dirty()).containsExactly(toChange);
+    }
+
+    @Test
+    void testComplexChangedAndResetDirty() {
+        DirtyPropertiesMap<Integer, PropertyHolder> dirtyMap = DirtyPropertiesMap.hashmap();
+        SomeComplexPropertyHolder toChange = new SomeComplexPropertyHolder(
+                20,
+                new SomePropertyHolder(10, "test")
+        );
+        dirtyMap.put(1, toChange);
+        dirtyMap.put(5, new SomePropertyHolder(15, "wording"));
+
+        toChange.number(20);
+        assertThat(dirtyMap.dirty()).isEmpty();
     }
 
 
@@ -55,5 +106,34 @@ public final class DirtyPropertiesMapTest {
         }
 
     }
+
+    static final class SomeComplexPropertyHolder implements PropertyHolder {
+
+        private int number;
+        private SomePropertyHolder someHolder;
+
+        SomeComplexPropertyHolder(final int number, final SomePropertyHolder someHolder) {
+            this.number = number;
+            this.someHolder = someHolder;
+        }
+
+        public void number(final int number) {
+            this.number = number;
+        }
+
+        public void someHolder(final SomePropertyHolder someHolder) {
+            this.someHolder = someHolder;
+        }
+
+        @Override
+        public @NonNull PropertySnapshot properties() {
+            return PropertySnapshot.of(
+                    Property.of("number", this.number),
+                    Property.of("someHolder", this.someHolder)
+            );
+        }
+
+    }
+
 
 }
