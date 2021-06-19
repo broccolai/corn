cccedfdd1c311478d9a2171a89146e80d5f0135a
TehBrian <tehbrian@outlook.com>
2021-06-19 15:23:25 -0400

    add BannerBuilder.java

---
+++ minecraft/paper/src/main/java/broccolai/corn/paper/item/BannerBuilder.java
@@ -0,0 +1,122 @@
+package broccolai.corn.paper.item;
+
+import org.bukkit.Material;
+import org.bukkit.block.banner.Pattern;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BannerMeta;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.checkerframework.checker.nullness.qual.NonNull;
+
+import java.util.List;
+
+/**
+ * Modifies {@link ItemStack}s that have an {@code ItemMeta} of {@link BannerMeta}.
+ */
+@SuppressWarnings("unused")
+public class BannerBuilder extends AbstractPaperItemBuilder<BannerBuilder, BannerMeta> {
+
+    private BannerBuilder(final @NonNull ItemStack item, final @NonNull BannerMeta meta) {
+        super(item, meta);
+    }
+
+    /**
+     * Creates a {@code BannerBuilder}.
+     *
+     * @param itemStack the {@code ItemStack} to base the builder off of
+     * @return instance of {@code BannerBuilder}
+     * @throws IllegalArgumentException if the {@code itemStack}'s {@code ItemMeta} is not the correct type
+     */
+    public static @NonNull BannerBuilder of(final @NonNull ItemStack itemStack) throws IllegalArgumentException {
+        final ItemMeta meta = itemStack.getItemMeta();
+
+        if (meta instanceof final BannerMeta newMeta) {
+            return new BannerBuilder(itemStack, newMeta);
+        } else {
+            throw new IllegalArgumentException("The ItemStack's ItemMeta must be of type "
+                    + BannerMeta.class.getSimpleName()
+                    + " but received ItemMeta of type "
+                    + meta.getClass().getSimpleName());
+        }
+    }
+
+    /**
+     * Creates a {@code SkullBuilder}.
+     *
+     * @param material the {@code Material} to base the builder off of
+     * @return instance of {@code SkullBuilder}
+     * @throws IllegalArgumentException if the {@code material} is not an obtainable item
+     */
+    public static @NonNull BannerBuilder ofType(final @NonNull Material material) throws IllegalArgumentException {
+        if (!material.isItem()) {
+            throw new IllegalArgumentException("The Material must be an obtainable item.");
+        }
+
+        return BannerBuilder.of(new ItemStack(material));
+    }
+
+    /**
+     * Gets the patterns.
+     *
+     * @return the patterns
+     */
+    public @NonNull List<Pattern> patterns() {
+        return this.itemMeta.getPatterns();
+    }
+
+    /**
+     * Sets the patterns.
+     *
+     * @param patterns the patterns
+     * @return the builder
+     */
+    public @NonNull BannerBuilder patterns(final @NonNull List<Pattern> patterns) {
+        this.itemMeta.setPatterns(patterns);
+        return this;
+    }
+
+    /**
+     * Gets a pattern.
+     *
+     * @param index the index
+     * @return the pattern
+     */
+    public @NonNull BannerBuilder getPattern(final @NonNull int index) {
+        this.itemMeta.getPattern(index);
+        return this;
+    }
+
+    /**
+     * Sets a pattern.
+     *
+     * @param index   the index
+     * @param pattern the pattern
+     * @return the builder
+     */
+    public @NonNull BannerBuilder setPattern(final @NonNull int index, final @NonNull Pattern pattern) {
+        this.itemMeta.setPattern(index, pattern);
+        return this;
+    }
+
+    /**
+     * Adds a pattern.
+     *
+     * @param pattern the pattern
+     * @return the builder
+     */
+    public @NonNull BannerBuilder addPattern(final @NonNull Pattern pattern) {
+        this.itemMeta.addPattern(pattern);
+        return this;
+    }
+
+    /**
+     * Removes a pattern.
+     *
+     * @param index the index
+     * @return the builder
+     */
+    public @NonNull BannerBuilder removePattern(final @NonNull int index) {
+        this.itemMeta.removePattern(index);
+        return this;
+    }
+
+}
