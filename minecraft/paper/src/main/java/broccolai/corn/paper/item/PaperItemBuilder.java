d305b65c4c5a2d2c56df084beca570a09930c78c
TehBrian <tehbrian@outlook.com>
2021-06-19 14:37:55 -0400

    add check for Material being an obtainable item

--- minecraft/paper/src/main/java/broccolai/corn/paper/item/PaperItemBuilder.java
+++ minecraft/paper/src/main/java/broccolai/corn/paper/item/PaperItemBuilder.java
@@ -35,8 +35,13 @@ public class PaperItemBuilder extends AbstractPaperItemBuilder<PaperItemBuilder,
      *
      * @param material the {@code Material} to base the builder off of
      * @return instance of {@code PaperItemBuilder}
+     * @throws IllegalArgumentException if the {@code Material} is not an obtainable item
      */
-    public static @NonNull PaperItemBuilder ofType(final @NonNull Material material) {
+    public static @NonNull PaperItemBuilder ofType(final @NonNull Material material) throws IllegalArgumentException {
+        if (!material.isItem()) {
+            throw new IllegalArgumentException("The Material must be an obtainable item.");
+        }
+
         return PaperItemBuilder.of(new ItemStack(material));
     }
 
