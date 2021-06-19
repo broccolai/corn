7d29a8ff5e94f3b89f5ba8f2e3d084767f673ed8
TehBrian <tehbrian@outlook.com>
2021-06-19 13:37:55 -0400

    add #material, #material(Material), #amount, #getEnchants, and #getFlags, rename #data to setData, #flags to #addFlags, and #enchant to #addEnchant

--- minecraft/spigot/src/main/java/broccolai/corn/spigot/item/AbstractItemBuilder.java
+++ minecraft/spigot/src/main/java/broccolai/corn/spigot/item/AbstractItemBuilder.java
@@ -1,5 +1,6 @@
 package broccolai.corn.spigot.item;
 
+import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
@@ -11,6 +12,7 @@ import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Modifies {@link ItemStack}s using methods that both Paper and Spigot share.
@@ -33,9 +35,38 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B, M>, M
     }
 
     /**
-     * Set the quantity of the {@code ItemStack}
+     * Get the {@link Material} of the {@code ItemStack}.
      *
-     * @param amount the {@code ItemStack}'s quantity
+     * @return the {@code Material}
+     */
+    public Material material() {
+        return this.itemStack.getType();
+    }
+
+    /**
+     * Set the {@link Material} of the {@code ItemStack}.
+     *
+     * @param material the {@code Material}
+     * @return the builder
+     */
+    public @NonNull B material(final Material material) {
+        this.itemStack.setType(material);
+        return (B) this;
+    }
+
+    /**
+     * Get the quantity of the {@code ItemStack}.
+     *
+     * @return the quantity
+     */
+    public int amount() {
+        return this.itemStack.getAmount();
+    }
+
+    /**
+     * Set the quantity of the {@code ItemStack}.
+     *
+     * @param amount the quantity
      * @return the builder
      */
     public @NonNull B amount(final int amount) {
@@ -44,30 +75,11 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B, M>, M
     }
 
     /**
-     * Add data to the {@code ItemStack}'s {@link org.bukkit.persistence.PersistentDataContainer}.
-     *
-     * @param key    the {@code NamespacedKey} to use
-     * @param type   the {@code PersistentDataType} to use
-     * @param object the data to set
-     * @param <T>   the primary object type of the data
-     * @param <Z>    the retrieve object type of the data
-     * @return the builder
-     */
-    public <T, Z> @NonNull B data(
-            final @NonNull NamespacedKey key,
-            final @NonNull PersistentDataType<T, Z> type,
-            final @NonNull Z object
-    ) {
-        this.itemMeta.getPersistentDataContainer().set(key, type, object);
-        return (B) this;
-    }
-
-    /**
      * Get data from the {@code ItemStack}'s {@link org.bukkit.persistence.PersistentDataContainer}.
      *
      * @param key  the {@code NamespacedKey} to use
      * @param type the {@code PersistentDatType to use}
-     * @param <T> the primary object type of the data
+     * @param <T>  the primary object type of the data
      * @param <Z>  the retrieve object type of the data
      * @return the data
      */
@@ -76,6 +88,25 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B, M>, M
             final @NonNull PersistentDataType<T, Z> type
     ) {
         return this.itemMeta.getPersistentDataContainer().get(key, type);
+    }
+
+    /**
+     * Add data to the {@code ItemStack}'s {@link org.bukkit.persistence.PersistentDataContainer}.
+     *
+     * @param key    the {@code NamespacedKey} to use
+     * @param type   the {@code PersistentDataType} to use
+     * @param object the data to set
+     * @param <T>    the primary object type of the data
+     * @param <Z>    the retrieve object type of the data
+     * @return the builder
+     */
+    public <T, Z> @NonNull B setData(
+            final @NonNull NamespacedKey key,
+            final @NonNull PersistentDataType<T, Z> type,
+            final @NonNull Z object
+    ) {
+        this.itemMeta.getPersistentDataContainer().set(key, type, object);
+        return (B) this;
     }
 
     /**
@@ -92,12 +123,21 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B, M>, M
     }
 
     /**
+     * Get the flags of the {@code ItemStack}.
+     *
+     * @return the flags
+     */
+    public @NonNull Set<ItemFlag> getFlags() {
+        return this.itemMeta.getItemFlags();
+    }
+
+    /**
      * Add flags to the {@code ItemStack}.
      *
-     * @param flags the {@code ItemFlag} to add
+     * @param flags the {@code ItemFlag}s to add
      * @return the builder
      */
-    public @NonNull B flags(final @NonNull ItemFlag... flags) {
+    public @NonNull B addFlags(final @NonNull ItemFlag... flags) {
         this.itemMeta.addItemFlags(flags);
         return (B) this;
     }
@@ -105,12 +145,21 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B, M>, M
     /**
      * Remove flags from the {@code ItemStack}.
      *
-     * @param flags the {@code ItemFlag} to add
+     * @param flags the {@code ItemFlag}s to add
      * @return the builder
      */
     public @NonNull B removeFlags(final @NonNull ItemFlag... flags) {
         this.itemMeta.removeItemFlags(flags);
         return (B) this;
+    }
+
+    /**
+     * Gets the {@code Enchantment}s of the {@code ItemStack}.
+     *
+     * @return the {@code Enchantment}s
+     */
+    public @NonNull Map<Enchantment, Integer> getEnchants() {
+        return new HashMap<>(this.enchantmentMap);
     }
 
     /**
@@ -120,7 +169,7 @@ public abstract class AbstractItemBuilder<B extends AbstractItemBuilder<B, M>, M
      * @param level       the level of the {@code Enchantment}
      * @return the builder
      */
-    public @NonNull B enchant(final @NonNull Enchantment enchantment, final int level) {
+    public @NonNull B addEnchant(final @NonNull Enchantment enchantment, final int level) {
         this.enchantmentMap.put(enchantment, level);
         return (B) this;
     }
