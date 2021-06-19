3021bd528779eee89f72a87fab206a4ea99a24ab
TehBrian <tehbrian@outlook.com>
2021-06-19 12:37:44 -0400

    add abstract superclasses for typesafe static getters

--- minecraft/paper/src/main/java/broccolai/corn/paper/PaperItemBuilder.java
+++ minecraft/paper/src/main/java/broccolai/corn/paper/PaperItemBuilder.java
@@ -1,19 +1,13 @@
 package broccolai.corn.paper;
 
-import broccolai.corn.spigot.ItemBuilder;
-import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
-
 @SuppressWarnings({"unchecked", "unused"})
-public class PaperItemBuilder<T extends PaperItemBuilder<T, M>, M extends ItemMeta> extends ItemBuilder<T, M> {
+public class PaperItemBuilder extends AbstractPaperItemBuilder<PaperItemBuilder, ItemMeta> {
 
     protected PaperItemBuilder(final @NonNull ItemStack itemStack, final @Nullable ItemMeta itemMeta) {
         super(itemStack, itemMeta);
@@ -25,8 +19,8 @@ public class PaperItemBuilder<T extends PaperItemBuilder<T, M>, M extends ItemMe
      * @param itemStack the ItemStack to base builder off of
      * @return instance of PaperItemBuilder
      */
-    public static PaperItemBuilder<?, ?> paper(final @NonNull ItemStack itemStack) {
-        return new PaperItemBuilder<>(itemStack, itemStack.getItemMeta());
+    public static PaperItemBuilder paper(final @NonNull ItemStack itemStack) {
+        return new PaperItemBuilder(itemStack, itemStack.getItemMeta());
     }
 
     /**
@@ -35,57 +29,8 @@ public class PaperItemBuilder<T extends PaperItemBuilder<T, M>, M extends ItemMe
      * @param material the material to base builder off of
      * @return instance of PaperItemBuilder
      */
-    public static PaperItemBuilder<?, ?> paper(final @NonNull Material material) {
+    public static PaperItemBuilder paper(final @NonNull Material material) {
         return PaperItemBuilder.paper(new ItemStack(material));
-    }
-
-    /**
-     * Set the name of the ItemStack.
-     *
-     * @param component the ItemStack's display name
-     * @return the builder
-     */
-    public @NonNull T name(final @NonNull Component component) {
-        this.itemMeta.displayName(component);
-        return (T) this;
-    }
-
-    /**
-     * Set the lore of the ItemStack.
-     *
-     * @param loreComponents the lines to set the ItemStacks lore to
-     * @return the builder
-     */
-    public @NonNull T loreComponents(final @NonNull List<Component> loreComponents) {
-        this.itemMeta.lore(loreComponents);
-        return (T) this;
-    }
-
-    /**
-     * Set the lore of the ItemStack.
-     *
-     * @param consumer the lines to set the ItemStacks lore to
-     * @return the builder
-     */
-    public @NonNull T loreComponents(final @NonNull Consumer<List<Component>> consumer) {
-        List<Component> lore = this.itemMeta.hasLore()
-                ? this.itemMeta.lore()
-                : new ArrayList<>();
-        consumer.accept(lore);
-
-        this.itemMeta.lore(lore);
-        return (T) this;
-    }
-
-    /**
-     * Set the lore of the ItemStack.
-     *
-     * @param loreComponents the lines to set the ItemStacks lore to
-     * @return the builder
-     */
-    public @NonNull T loreComponents(final @NonNull Component... loreComponents) {
-        this.itemMeta.lore(List.of(loreComponents));
-        return (T) this;
     }
 
 }
--- minecraft/spigot/src/main/java/broccolai/corn/spigot/ItemBuilder.java
+++ minecraft/spigot/src/main/java/broccolai/corn/spigot/ItemBuilder.java
@@ -1,39 +1,16 @@
 package broccolai.corn.spigot;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataType;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Consumer;
-
 @SuppressWarnings({"unchecked", "unused"})
-public class ItemBuilder<T extends ItemBuilder<T, M>, M extends ItemMeta> {
+public class ItemBuilder extends AbstractItemBuilder<ItemBuilder, ItemMeta> {
 
-    protected final @NonNull ItemStack itemStack;
-    protected final @NonNull M itemMeta;
-
-    protected final @NonNull Map<Enchantment, Integer> enchantmentMap;
-
-    protected ItemBuilder(final @NonNull ItemStack itemStack, final @Nullable ItemMeta itemMeta) {
-        this.itemStack = itemStack.clone();
-        this.itemMeta = itemMeta != null
-                ? itemMeta
-                : Objects.requireNonNull(
-                        Bukkit.getItemFactory().getItemMeta(itemStack.getType())
-                );
-        this.enchantmentMap = new HashMap<>(this.itemMeta.getEnchants());
+    private ItemBuilder(final @NonNull ItemStack itemStack, final @Nullable ItemMeta itemMeta) {
+        super(itemStack, itemMeta);
     }
 
     /**
@@ -42,8 +19,8 @@ public class ItemBuilder<T extends ItemBuilder<T, M>, M extends ItemMeta> {
      * @param itemStack the ItemStack to base builder off of
      * @return instance of ItemBuilder
      */
-    public static ItemBuilder<?, ?> spigot(final @NonNull ItemStack itemStack) {
-        return new ItemBuilder<>(itemStack, itemStack.getItemMeta());
+    public static ItemBuilder spigot(final @NonNull ItemStack itemStack) {
+        return new ItemBuilder(itemStack, itemStack.getItemMeta());
     }
 
     /**
@@ -52,144 +29,8 @@ public class ItemBuilder<T extends ItemBuilder<T, M>, M extends ItemMeta> {
      * @param material the material to base builder off of
      * @return instance of ItemBuilder
      */
-    public static ItemBuilder<?, ?> spigot(final @NonNull Material material) {
+    public static ItemBuilder spigot(final @NonNull Material material) {
         return ItemBuilder.spigot(new ItemStack(material));
-    }
-
-    /**
-     * Set the name of the ItemStack.
-     *
-     * @param name the ItemStack's display name
-     * @return the builder
-     */
-    public @NonNull T name(final @Nullable String name) {
-        this.itemMeta.setDisplayName(name);
-        return (T) this;
-    }
-
-    /**
-     * Set the quantity of the ItemStack.
-     *
-     * @param amount the ItemStacks quantity
-     * @return the builder
-     */
-    public @NonNull T amount(final int amount) {
-        this.itemStack.setAmount(amount);
-        return (T) this;
-    }
-
-    /**
-     * Set the lore of the ItemStack.
-     *
-     * @param lines the lines to set the ItemStacks lore to
-     * @return the builder
-     */
-    public @NonNull T lore(final @Nullable List<String> lines) {
-        this.itemMeta.setLore(lines);
-        return (T) this;
-    }
-
-    /**
-     * Set the lore of the ItemStack.
-     *
-     * @param consumer the lines to set the ItemStacks lore to
-     * @return the builder
-     */
-    public @NonNull T lore(final @NonNull Consumer<List<String>> consumer) {
-        List<String> lore = this.itemMeta.hasLore() ? this.itemMeta.getLore() : new ArrayList<>();
-        consumer.accept(lore);
-
-        this.itemMeta.setLore(lore);
-        return (T) this;
-    }
-
-    /**
-     * Set the lore of the ItemStack.
-     *
-     * @param lines the lines to set the ItemStack's lore to
-     * @return the builder
-     */
-    public @NonNull T lore(final @NonNull String... lines) {
-        this.itemMeta.setLore(List.of(lines));
-        return (T) this;
-    }
-
-    /**
-     * Add data to the items PersistentDataContainer
-     *
-     * @param key    the NamespacedKey to use
-     * @param type   the data type to use
-     * @param object data to set
-     * @param <T0>   the primary object type of data
-     * @param <Z>    the retrieve object type of data
-     * @return the builder
-     */
-    public <T0, Z> @NonNull T data(
-            final @NonNull NamespacedKey key,
-            final @NonNull PersistentDataType<T0, Z> type,
-            final @NonNull Z object
-    ) {
-        this.itemMeta.getPersistentDataContainer().set(key, type, object);
-        return (T) this;
-    }
-
-    /**
-     * Remove a persistent data container key from the item
-     *
-     * @param key the NamespacedKey to use
-     * @return the builder
-     */
-    public @NonNull T removeData(
-            final @NonNull NamespacedKey key
-    ) {
-        this.itemMeta.getPersistentDataContainer().remove(key);
-        return (T) this;
-    }
-
-    /**
-     * Add flags to an ItemStack.
-     *
-     * @param flags the ItemFlag to add the the ItemStack
-     * @return the builder
-     */
-    public @NonNull T flags(final @NonNull ItemFlag... flags) {
-        this.itemMeta.addItemFlags(flags);
-        return (T) this;
-    }
-
-    /**
-     * Delete flags from an ItemStack.
-     *
-     * @param flags the ItemFlag to add the the ItemStack
-     * @return the builder
-     */
-    public @NonNull T deleteFlags(final @NonNull ItemFlag... flags) {
-        this.itemMeta.removeItemFlags(flags);
-        return (T) this;
-    }
-
-    /**
-     * Adds an Enchantment to an ItemStack.
-     *
-     * @param enchantment the Enchantment to add to the ItemStack
-     * @param level       the Level of the Enchantment
-     * @return the builder
-     */
-    public @NonNull T enchant(final @NonNull Enchantment enchantment, final int level) {
-        this.enchantmentMap.put(enchantment, level);
-        return (T) this;
-    }
-
-    /**
-     * Build ItemStack from set properties.
-     *
-     * @return the built ItemStack
-     */
-    public @NonNull ItemStack build() {
-        this.itemStack.setItemMeta(this.itemMeta);
-        this.itemStack.addUnsafeEnchantments(this.enchantmentMap);
-
-        return this.itemStack.clone();
     }
 
 }
