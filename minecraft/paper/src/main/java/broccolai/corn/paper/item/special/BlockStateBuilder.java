8fa1f8c4276716806f03af7aa3cc6c9efadad14c
TehBrian <tehbrian@outlook.com>
2021-07-14 14:45:12 -0400

    fix incorrect meta type in BlockStateBuilder
    oh god, i just realized the burden of maintenance
    that having two boilerplate functions
    in a dozen and a half builders creates. aaaa
    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- minecraft/paper/src/main/java/broccolai/corn/paper/item/special/BlockStateBuilder.java
+++ minecraft/paper/src/main/java/broccolai/corn/paper/item/special/BlockStateBuilder.java
@@ -3,45 +3,45 @@ package broccolai.corn.paper.item.special;
 import broccolai.corn.paper.item.AbstractPaperItemBuilder;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.CompassMeta;
+import org.bukkit.inventory.meta.BlockStateMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
 /**
- * Modifies {@link ItemStack}s that have an {@code ItemMeta} of {@link CompassMeta}.
+ * Modifies {@link ItemStack}s that have an {@code ItemMeta} of {@link BlockStateMeta}.
  */
 @SuppressWarnings("unused")
-public final class BlockStateBuilder extends AbstractPaperItemBuilder<BlockStateBuilder, CompassMeta> {
+public final class BlockStateBuilder extends AbstractPaperItemBuilder<BlockStateBuilder, BlockStateMeta> {
 
-    private BlockStateBuilder(final @NonNull ItemStack itemStack, final @NonNull CompassMeta itemMeta) {
+    private BlockStateBuilder(final @NonNull ItemStack itemStack, final @NonNull BlockStateMeta itemMeta) {
         super(itemStack, itemMeta);
     }
 
     /**
-     * Creates a {@code CompassBuilder}.
+     * Creates a {@code BlockStateBuilder}.
      *
      * @param itemStack the {@code ItemStack} to base the builder off of
-     * @return instance of {@code CompassBuilder}
+     * @return instance of {@code BlockStateBuilder}
      * @throws IllegalArgumentException if the {@code itemStack}'s {@code ItemMeta} is not the correct type
      */
     public static @NonNull BlockStateBuilder of(final @NonNull ItemStack itemStack) throws IllegalArgumentException {
         final ItemMeta meta = itemStack.getItemMeta();
 
-        if (meta instanceof final CompassMeta newMeta) {
+        if (meta instanceof final BlockStateMeta newMeta) {
             return new BlockStateBuilder(itemStack, newMeta);
         } else {
             throw new IllegalArgumentException("The ItemStack's ItemMeta must be of type "
-                    + CompassMeta.class.getSimpleName()
+                    + BlockStateMeta.class.getSimpleName()
                     + " but received ItemMeta of type "
                     + meta.getClass().getSimpleName());
         }
     }
 
     /**
-     * Creates a {@code CompassBuilder}.
+     * Creates a {@code BlockStateBuilder}.
      *
      * @param material the {@code Material} to base the builder off of
-     * @return instance of {@code CompassBuilder}
+     * @return instance of {@code BlockStateBuilder}
      * @throws IllegalArgumentException if the {@code material} is not an obtainable item,
      *                                  or if the {@code material}'s {@code ItemMeta} is not the correct type
      */
