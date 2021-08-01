fbe73860d240603d704f1c1536afcba6879880b2
TehBrian <tehbrian@outlook.com>
2021-07-31 22:12:00 -0400

    add methods for TropicalFishBucketBuilder

--- minecraft/paper/src/main/java/broccolai/corn/paper/item/special/TropicalFishBucketBuilder.java
+++ minecraft/paper/src/main/java/broccolai/corn/paper/item/special/TropicalFishBucketBuilder.java
@@ -2,7 +2,9 @@ package broccolai.corn.paper.item.special;
 
 import broccolai.corn.paper.item.AbstractPaperItemBuilder;
 import broccolai.corn.spigot.item.AridUtil;
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.entity.TropicalFish;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.TropicalFishBucketMeta;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -38,6 +40,76 @@ public final class TropicalFishBucketBuilder extends AbstractPaperItemBuilder<Tr
      */
     public static @NonNull TropicalFishBucketBuilder ofType(final @NonNull Material material) throws IllegalArgumentException {
         return TropicalFishBucketBuilder.of(AridUtil.getItem(material));
+    }
+
+    /**
+     * Gets the {@code Pattern}.
+     *
+     * @return the pattern
+     */
+    public TropicalFish.@NonNull Pattern pattern() {
+        return this.itemMeta.getPattern();
+    }
+
+    /**
+     * Sets the {@code Pattern}.
+     *
+     * @param pattern the pattern
+     * @return the builder
+     */
+    public @NonNull TropicalFishBucketBuilder pattern(final TropicalFish.@NonNull Pattern pattern) {
+        this.itemMeta.setPattern(pattern);
+        return this;
+    }
+
+    /**
+     * Gets the pattern color.
+     *
+     * @return the pattern color
+     */
+    public @NonNull DyeColor patternColor() {
+        return this.itemMeta.getPatternColor();
+    }
+
+    /**
+     * Sets the pattern color.
+     *
+     * @param patternColor the pattern color
+     * @return the builder
+     */
+    public @NonNull TropicalFishBucketBuilder patternColor(final @NonNull DyeColor patternColor) {
+        this.itemMeta.setPatternColor(patternColor);
+        return this;
+    }
+
+    /**
+     * Gets the body color.
+     *
+     * @return the body color
+     */
+    public @NonNull DyeColor bodyColor() {
+        return this.itemMeta.getBodyColor();
+    }
+
+    /**
+     * Sets the body color.
+     *
+     * @param bodyColor the body color
+     * @return the builder
+     */
+    public @NonNull TropicalFishBucketBuilder bodyColor(final @NonNull DyeColor bodyColor) {
+        this.itemMeta.setBodyColor(bodyColor);
+        return this;
+    }
+
+    /**
+     * Checks whether a variant tag exists.
+     * If true, a specific fish will be spawned.
+     *
+     * @return whether a variant tag exists
+     */
+    public boolean variant() {
+        return this.itemMeta.hasVariant();
     }
 
 }
