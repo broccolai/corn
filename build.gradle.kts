e8866f1014fa0c82f5319fff8b60d58d12f6f60e
TehBrian <tehbrian@protonmail.com>
2022-06-18 15:05:10 -0400

    3.0.1-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "broccolai.corn"
-version = "3.0.0-SNAPSHOT"
+version = "3.0.1-SNAPSHOT"
 description = "extremely opinionated mostly personal java utilities"
 
 subprojects {
